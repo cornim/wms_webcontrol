8cc701d276d98fb6a2b24851f656d1bcdad50991
Dr. Cornelius Mund <cornelius.mund@gmail.com>
2019-06-11 20:13:33 +0200

    Moved dependencies form requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cornim/wms_webcontrol.git",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: LGPLv3",
         "Operating System :: OS Independent",
     ],
 )
